297b177b1b91427fcfba297117b018cde3344409
Nigee Hettige <nigeehettige20@gmail.com>
2024-05-27 00:21:57 +0530

    modification 1.0

---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {hightlightsSlides} from '../constants'
+
+const VideoCarousel = () => {
+  return (
+    <>
+      <div className='flex items-center'>
+        {hightlightsSlides.map((list,i)=>(
+            <div key={list.id} id='slider' className='sm:pr-20 pr-10'>
+                <div className="video-carousel_container">
+                    <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black ">
+                        <video id='video' muted playsInline={true} preload='auto'>
+                            <source src={list.video} type='video/mp4'/>
+                        </video>
+                    </div>
+                    <div className="absolute top-12 left-[5%] z-10">
+                        {list.textLists.map((text)=>(
+                            <p key={text} className='text-xl sm:text-2xl font-medium'>
+                                {text}
+                            </p>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default VideoCarousel
