61c6916420cd417b545e606355ce33392c6e0094
Nigee Hettige <nigeehettige20@gmail.com>
2024-05-28 15:36:41 +0530

    modification 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
+import Model from "./components/Model"
 
 
 
@@ -13,6 +14,7 @@ function App() {
         <Navbar/>
         <Hero/>
         <Highlights/>
+        <Model/>
       </main>
     </>
   )
